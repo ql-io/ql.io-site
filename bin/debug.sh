e54cf6d968ad3343298a10c800ff6c5c9d3ba34e
Subbu Allamaraju <subbu@ebaysf.com>
2011-11-05 19:21:00 -0700

    Use 3000 and 3001

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node --debug lib/site.js --tables $PWD/tables/ --routes $PWD/routes/ --config $PWD/config/dev.json --port 8080
+node --debug lib/site.js --tables $PWD/tables/ --routes $PWD/routes/ --config $PWD/config/dev.json --port 3000
--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node lib/site.js --cluster --tables $PWD/tables/ --routes $PWD/routes/ --config $PWD/config/dev.json --port 8080 --monPort 8081
+node lib/site.js --cluster --tables $PWD/tables/ --routes $PWD/routes/ --config $PWD/config/dev.json --port 3000 --monPort 3001
