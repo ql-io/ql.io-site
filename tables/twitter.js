bba78ee34c748574453524993d15927816e75ccf
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2011-12-05 16:31:08 +0900

    Change the page parameter into integer more than 1

--- tables/twitter.js
+++ tables/twitter.js
@@ -1,7 +1,7 @@
 exports['patch uri'] = function(options) {
     var statement = options.statement, uri = options.uri;
     if(statement.offset && statement.limit) {
-        uri.setParam('page', statement.offset / statement.limit);
+        uri.setParam('page', parseInt(statement.offset / statement.limit)+1);
     }
     uri.removeEmptyParams();
     return uri;
